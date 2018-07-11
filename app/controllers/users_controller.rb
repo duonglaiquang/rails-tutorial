648d6e9c437ceded01a9aa2d60a41807d6cb3bef
duonglaiquang <duonglaiquang@gmail.com>
2018-07-11 21:16:50 +0700

    Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t("application.user_error")
+    redirect_to home_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:new] = t "users.new.success"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
