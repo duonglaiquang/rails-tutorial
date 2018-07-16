6afc8d9d11f7dbfb69103ba6d6384de5d8b5462d
duonglaiquang <duonglaiquang@gmail.com>
2018-07-16 15:24:44 +0700

    Password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: t("mailer.title")
   end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("password_resets.title")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   validates :name, presence: true,
             length: {maximum: Settings.user_setting.name_length_max}
 
-  validates :password, presence: true,
+  validates :password, allow_nil: true, presence: true,
             length: {minimum: Settings.user_setting.password_length_min}
 
   class << self
@@ -48,12 +48,25 @@ class User < ApplicationRecord
   end
 
   def activate
-    update_attributes activated: true
-    update_attributes activated_at: Time.zone.now
+    update_attributes activated: true, activated_at: Time.zone.now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attributes reset_sent_at: Time.zone.now,
+                      reset_digest: User.digest(reset_token)
   end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.user_setting.pw_expired_time.hours.ago
   end
 
   private
